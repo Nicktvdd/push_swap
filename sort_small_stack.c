664977306e3fc6ae1dbcbc2279dc48cdd85c365a
Nick van den Dungen <nicktvdd@hotmail.com>
2023-05-26 12:47:43 +0300

    optimized sort5, on to radix

--- sort_small_stack.c
+++ sort_small_stack.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 11:58:11 by nvan-den          #+#    #+#             */
-/*   Updated: 2023/05/26 12:27:07 by nvan-den         ###   ########.fr       */
+/*   Updated: 2023/05/26 12:38:13 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void sort_3(stack* a_stack)
     }
 }
 
-/* void sort_5(stack* a_stack, stack* b_stack)
+void sort_5(stack* a_stack, stack* b_stack)
 {
 	pb(a_stack, b_stack);
 	pb(a_stack, b_stack);
@@ -60,11 +60,10 @@ void sort_3(stack* a_stack)
 		pa(a_stack, b_stack);
 		ra(a_stack);
 	}
-} */
+}
 
 
-
-void sort_5(stack* a_stack, stack* b_stack)
+/* void sort_5(stack* a_stack, stack* b_stack)
 {
 	int	i;
 
@@ -80,9 +79,7 @@ void sort_5(stack* a_stack, stack* b_stack)
 	ft_printf("peek b_stack: %i\n", peek(b_stack));
 	while (peek(b_stack))
 			pa(a_stack, b_stack);
-	//pa(a_stack, b_stack);
-			ft_printf("peek b_stack: %i\n", peek(b_stack));
-}
+} */
 
 stack* sort_small_stack(int size, stack* a_stack, stack* b_stack)
 {
