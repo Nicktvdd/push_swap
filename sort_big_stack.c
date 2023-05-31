5246173c25f9152dd75e6061703f503e28752d4b
Nick van den Dungen <nicktvdd@hotmail.com>
2023-05-31 16:54:03 +0300

    increment i was incorrect

--- sort_big_stack.c
+++ sort_big_stack.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 15:22:57 by nvan-den          #+#    #+#             */
-/*   Updated: 2023/05/31 16:50:02 by nvan-den         ###   ########.fr       */
+/*   Updated: 2023/05/31 16:53:42 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void sort_big_stack(stack* a_stack, stack* b_stack)
 	j = 0;
 	while ((max_num >> max_bits) != 0) 
 		++max_bits;
-	while (i++ < max_bits)
+	while (i < max_bits)
 	{
 		while (j++ < size)
 		{
@@ -44,5 +44,6 @@ void sort_big_stack(stack* a_stack, stack* b_stack)
 		j = 0;
 		while (peek(b_stack))
 			pa(a_stack, b_stack);
+		i++;
 	}
 }
