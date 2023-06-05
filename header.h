519e0c12b3d73d81365157b727e9d38df0369734
Nick van den Dungen <nicktvdd@hotmail.com>
2023-06-05 14:11:52 +0300

    stack = t_stack

--- header.h
+++ header.h
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 12:54:13 by nvan-den          #+#    #+#             */
-/*   Updated: 2023/06/05 14:08:51 by nvan-den         ###   ########.fr       */
+/*   Updated: 2023/06/05 14:11:37 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,16 +40,16 @@ void	rr(t_stack *a_stack, t_stack *b_stack);
 void	rra(t_stack *a_stack);
 void	rrb(t_stack *b_stack);
 void	rrr(t_stack *a_stack, t_stack *b_stack);
-stack	*parse(int argc, char **argv);
+t_stack	*parse(int argc, char **argv);
 void	sort_big_stack(t_stack *a_stack, t_stack *b_stack);
-stack	*sort_small_stack(int argc, t_stack *a_stack, t_stack *b_stack);
+t_stack	*sort_small_stack(int argc, t_stack *a_stack, t_stack *b_stack);
 void	push(t_stack *s, int value);
 int		pop(t_stack *s);
 int		peek(t_stack *s);
 int		is_empty(t_stack *s);
 void	print_stack(t_stack *s);
 void	print_stackb(t_stack *s);
-stack	*create_stack(void);
+t_stack	*create_stack(void);
 void	create_index(t_stack *a_stack);
 
 #endif
